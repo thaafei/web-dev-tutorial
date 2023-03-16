4246029bb65e04c70e78e96f454cbd199975e52e
Fei <50682656+thaafei@users.noreply.github.com>
2023-03-16 16:22:21 +0000

    changed alignment of button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,jsonify  #flask=module, Flask = class
+from flask import Flask, render_template, jsonify  #flask=module, Flask = class
 
 app = Flask(__name__)  #creating object of Flask
 
